a52174e090154fcd5febcd669045c5001deb9efc
Prayas-sharmaa <x24280585@student.ncirl.ie>
2025-12-05 13:33:32 +0000

    ansible

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+
+resource "aws_elastic_beanstalk_application" "store_app" {
+  name        = "store-management-app"
+  description = "Store management web app"
+}
+
+
+
+resource "aws_elastic_beanstalk_environment" "store_env" {
+  name                = "store-management-env"
+  cname_prefix        = "storewebapp" 
+  application         = aws_elastic_beanstalk_application.store_app.name
+  solution_stack_name = "64bit Amazon Linux 2023 v4.8.0 running Python 3.14"
+    setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = "LabInstanceProfile"
+  }
+  
+
+  # Service role
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "ServiceRole"
+    value     = "LabRole"
+  }
+
+  # EC2 key pair
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "EC2KeyName"
+    value     = "vockey"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "app_name" {
+  default = "storemanagement"
+}
+
+variable "env_name" {
+  default = "storemanagement-env"
+}
